1ff5b99691ce82bb163038c597509316fc128ee3
hoangha5 <hoanghan0613@gmail.com>
2026-01-31 13:29:17 -0500

    merge

--- finlife/src/scenes/CharacterSelect.js
+++ finlife/src/scenes/CharacterSelect.js
@@ -48,7 +48,7 @@ export class CharacterSelect extends Phaser.Scene {
     girlSprite.play("girlIdle")
     girlSprite.setInteractive({ useHandCursor: true })
     girlSprite.on("pointerdown", () => {
-      this.scene.start("GameScene", { character: "A" })
+      this.selectCharacter(girlSprite, boySprite, "A")
     })
 
     // Character B - Boy (right) - click to select
@@ -57,7 +57,30 @@ export class CharacterSelect extends Phaser.Scene {
     boySprite.play("boyIdle")
     boySprite.setInteractive({ useHandCursor: true })
     boySprite.on("pointerdown", () => {
-      this.scene.start("GameScene", { character: "B" })
+      this.selectCharacter(girlSprite, boySprite, "B")
+    })
+  }
+
+  selectCharacter(selectedSprite, otherSprite, character) {
+    // Disable further clicks
+    selectedSprite.removeInteractive()
+    otherSprite.removeInteractive()
+
+    // Draw highlight frame around selected character
+    const pad = 8
+    const w = 120 + pad * 2
+    const h = 120 + pad * 2
+    const x = selectedSprite.x - w / 2
+    const y = selectedSprite.y - h / 2
+
+    const highlight = this.add.graphics()
+    highlight.lineStyle(4, 0x4caf50)
+    highlight.strokeRect(x, y, w, h)
+    highlight.setDepth(10)
+
+    // Brief delay to show selection, then start game
+    this.time.delayedCall(400, () => {
+      this.scene.start("GameScene", { character })
     })
   }
 }
