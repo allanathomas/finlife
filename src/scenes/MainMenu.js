8f46cb82f3ac618867d343b9a1423905c4e7a4c4
Mel <mellyng2611@gmail.com>
2026-01-31 14:12:37 -0500

    mainMenuDone

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -5,22 +5,26 @@ export class MainMenu extends Phaser.Scene {
     super("MainMenu")
   }
 
+  preload() {
+    this.load.image('background', 'pictures/mainMenu.png');
+    this.load.image('playButton', 'pictures/PLAY.png');    this.load.image('decoration', 'pictures/balloon.png');  }
+
   create() {
-    this.add.text(400, 150, "FINLIFE", {
+    this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'background').setOrigin(0.5).setDisplaySize(this.cameras.main.width, this.cameras.main.height).setAlpha(0.7);
+
+    this.add.text(this.cameras.main.centerX, this.cameras.main.height * 0.4, "FINLIFE", {
       fontSize: "128px",
       color: "#ffffff",
+      fontFamily: 'monospace',
     }).setOrigin(0.5)
 
-    const playButton = this.add.text(400, 350, "PLAY", {
-      fontSize: "80px",
-      color: "#ffffff",
-      backgroundColor: "#4caf50",
-      padding: { x: 20, y: 10 },
-    }).setOrigin(0.5)
+    const playButton = this.add.image(this.cameras.main.centerX, this.cameras.main.height * 0.6, 'playButton').setOrigin(0.5).setScale(0.25);
 
     playButton.setInteractive()
     playButton.on("pointerdown", () => {
       this.scene.start("CharacterSelect")
     })
+
+    this.add.image(this.cameras.main.centerX + 400, this.cameras.main.height * 0.6, 'decoration').setOrigin(0.5).setScale(0.4);
   }
 }
