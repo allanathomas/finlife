6da6a70e86b1e6b82afa9cf592a72444bff6bb23
allana <116528863+allanathomas@users.noreply.github.com>
2026-01-31 20:28:27 -0500

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -60,12 +60,13 @@ export class GameScene extends Phaser.Scene {
       "Have fun!"
     ])
 
-    this.add.image(400, 400, "nextButton")
-      .setScale(0.2)
-      .setInteractive()
-      .on("pointerdown", () => {
-      this.scene.start("HomeScene")
-    })
+    // Add NEXT image button
+    const nextBtn = this.add.image(this.cameras.main.width - 80, this.cameras.main.height - 60, "nextButton")
+      .setScale(0.22)
+      .setInteractive();
+    nextBtn.on("pointerdown", () => {
+      this.scene.start("HomeScene");
+    });
   }
 
   drawBars() {
