d9e895ac86daabe16e44cf02599f7d991bfd7ee5
Mel <mellyng2611@gmail.com>
2026-01-31 20:46:00 -0500

    change12

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -7,16 +7,19 @@ export class MainMenu extends Phaser.Scene {
 
   preload() {
     this.load.image('background', 'pictures/mainMenu.png');
-    this.load.image('playButton', 'pictures/PLAY.png');    this.load.image('decoration', 'pictures/balloon.png');  }
+    this.load.image('playButton', 'pictures/PLAY.png');
+    this.load.image('decoration', 'pictures/balloon.png');
+    this.load.image('finlifeLogo', 'pictures/finlife.png');
+  }
 
   create() {
+
     this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'background').setOrigin(0.5).setDisplaySize(this.cameras.main.width, this.cameras.main.height).setAlpha(0.7);
 
-    this.add.text(this.cameras.main.centerX, this.cameras.main.height * 0.4, "FINLIFE", {
-      fontSize: "128px",
-      color: "#ffffff",
-      fontFamily: 'monospace',
-    }).setOrigin(0.5)
+    // Add the finlife logo image instead of text
+    this.add.image(this.cameras.main.centerX, this.cameras.main.height * 0.4, 'finlifeLogo')
+      .setOrigin(0.5)
+      .setScale(0.95); // Adjust scale as needed for your logo size
 
     const playButton = this.add.image(this.cameras.main.centerX, this.cameras.main.height * 0.6, 'playButton').setOrigin(0.5).setScale(0.25);
 
